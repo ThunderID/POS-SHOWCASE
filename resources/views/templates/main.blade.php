c64241c0a04dd6f2c0b1322664485bb5eef8c8e6
budi7 <budi-purnomo@outlook.com>
2019-02-18 14:26:36 +0700

    split views

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.base')
+
+@section('template_content')
+    @include('templates.components.header')
+    <main class="ps-main">
+        @stack('content')
+    </main>
+    @stack('modals')
+@stop
+
+
+@section('template_script')
+	<script type="text/javascript">
+		function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
+		// use like
+		r(function(){
+			@stack('scripts')
+		});	
+	</script>
+
+	@stack('script_plugins')
+@stop
